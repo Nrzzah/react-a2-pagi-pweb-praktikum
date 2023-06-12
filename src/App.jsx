468e33326a95514c8b18c072b4636999002d30ad
JoenDev03 <aadscreet@gmail.com>
2023-06-12 16:53:38 +0700

    update button pertemuan ke 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Style from './App.module.css'
+import Button from './components/Button'
 function App() {
   return (
     <>
@@ -15,7 +16,16 @@ function App() {
 <a href="#" className={Style.link}>About Us</a>
     </div>
     </nav>
-      <h1>Praktikum A2 Pagi</h1>
+      <h1 
+      className="text-2xl hover:font-bold hover:cursor-pointer active:underline"
+      >Praktikum A2 Pagi
+      </h1>
+      <Button className=" bg-green-400 text-white hover:bg-green-500">
+        Button 1
+        </Button>
+        <Button className=" bg-red-400 text-white hover:bg-red-500">
+        Button 2
+        </Button>
     </>
   )
 }
